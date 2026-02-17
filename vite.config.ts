896ea1dfa4fbc6ab0872a13dd57665965cfcc54f
Michael A. <nomicflux@gmail.com>
2026-02-17 09:54:55 -0600

    publish via github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const repoName = "entangled-graphs";
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : "/",
   plugins: [vue()],
 });
